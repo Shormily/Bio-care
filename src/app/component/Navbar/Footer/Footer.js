7e2c39d46883be8c6ee919e7e7bd60e3e3d1abec
Shormily <shormilyrais@gmail.com>
2024-08-28 19:58:48 +0600

    fot

--- src/app/component/Navbar/Footer/Footer.js
+++ src/app/component/Navbar/Footer/Footer.js
@@ -9,70 +9,112 @@ const Footer = () => {
     <>
       {/* <JoinOur/> */}
       {/* Footer Section */}
-     
+
       <div className="bg-[#023020] justify-center items-center m-auto flex  font-inriaSans pt-12 pb-8 ">
         <div
           className="max-w-[1550px] justify-center items-center m-auto px-6  pt-12 pb-8
       "
         >
           {/* max-w-[1120px] m-auto pt-3 pb-3  flex items-center justify-between mx-auto */}
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-24  ">
-            <div className="text-white">
-                          <div className="flex">  <Image
-                               width={50}
-                               height={50}
+          <div className="grid lg:grid-cols-3 pt-7 md:grid-cols-2 sm:grid-cols-1 gap-24  ">
+            <div className="text-white ">
+              <div className="flex gap-4">  <Image
+                width={70}
+                height={70}
                 className="imgse text-white"
                 src='/asset/logo.png'
                 alt=""
               />
-              <p className=" text-3xl font-medium mt-3 ">BioCare</p></div>
-            
-              <p className="py-2 text-[18px] font-light text-gray-200 ">
+                <p className=" text-4xl font-bold mt-3 ">BioCare</p></div>
+              <hr className="mt-4 p-2" />
+              <p className="py-2 text-[16px] font-light text-gray-200 ">
                 Experience the joy of endless possibilities – one click at a
                 time. Welcome to a world where happiness is just a box away.
               </p>
+              <hr className="mt-4 p-2 " />
+              <div className="text-gray-200   text-semibold font-light text-[16px]">
+                <p className="py-2">About US</p>
+                <p className="py-2">Terms & Condition</p>
+                <p className="py-2">Privacy Policy</p>
+                <p className="py-2">Contact Us </p>
+                <p className="py-2">Carrer </p>
+              </div>
             </div>
 
             <div className="text-white">
-              <p className="font-semibold mb-2 text-[22px]">Information</p>
-              <div className="text-gray-200 pt-5 font-light text-[18px]">
-                <p>Sign Up</p>
-                <p>Newsletter</p>
-                <p>Blogs</p>
-                <p>Trading</p>
+              <p className="font-semibold mb-2 text-[22px] ">Information</p>
+              <div
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#fff",
+                  height: "2px",
+                  width: "60px"
+                }}
+              />
+              <div className=" pt-5    text-[16px]">
+                {" "} <p className="py-2 text-gray-200 mt-8">Email: biocarebdteam@gmail.com</p>
+                <p className="py-2 text-gray-200">Phone : 01600-538143</p>
+                <p className="py-2">Address: Sylhet</p>
+                <p className="py-2">Level: 3, Road: 14, Block: C 180/6,
+                  Abdullah Park, 23/C Banasree Main Rd,
+                  Dhaka-1219</p>
+                <p className="py-2">Trade License: TRAD/DNCC/131766/2022</p>
+
+                <div className="flex justify-between  text-white  m-auto ">
+                  <p className="mt-8">
+                    {/* <PiInstagramLogoDuotone size={2} className="icn" /> */}
+                    <i className="fa-brands fa-facebook  shadow icn outline outline-[0.1px] outline-[#F0ECE5]"></i>{" "}
+                    <i className="fa-brands fa-instagram icn shadow outline outline-[0.1px] outline-[#F0ECE5]"></i>
+                    <i className="fa-brands fa-linkedin icn shadow outline outline-[0.1px] outline-[#F0ECE5]"></i>{" "}
+                  </p>
+
+                </div>
               </div>
             </div>
             <div className="text-white">
-              <p className="font-semibold mb-2 text-[22px]">Platform</p>
-              <div className="text-gray-200 pt-5 font-light text-[18px]">
-                {" "}
-                <p>About</p>
-                <p>Contract</p>
-                <p>Collection</p>
-                <p>Product</p>
+              <p className="font-semibold mb-2 text-[22px]">Let's talk about health!</p>
+              <div
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#fff",
+                  height: "2px",
+                  width: "60px"
+                }}
+              />
+              <div className="text-gray-200 pt-5 font-light text-[18px] mb-5">
+
+                <p className="text-white"></p>
+                <Image
+                  width={1000}
+                  height={1000}
+                  className="imgse text-white mt-4"
+                  src='/asset/ssl.png'
+                  alt=""
+                />
+
               </div>
             </div>
+
+
+
             <div>
-             
+
             </div>
 
           </div>
 
-          <hr className="" />
-          <div className="flex justify-between  text-white  m-auto ">
-            <p className="mt-8">
-              {/* <PiInstagramLogoDuotone size={2} className="icn" /> */}
-              <i className="fa-brands fa-facebook  shadow icn outline outline-[0.1px] outline-[#F0ECE5]"></i>{" "}
-              <i className="fa-brands fa-instagram icn shadow outline outline-[0.1px] outline-[#F0ECE5]"></i>
-              <i className="fa-brands fa-linkedin icn shadow outline outline-[0.1px] outline-[#F0ECE5]"></i>{" "}
-            </p>
-            <p className=" text-[18px] pb-6 justify-end items-end flex">
-              2024 All rights reserved. &copy;2024 BioCare Bangladesh
-            </p>
-          </div>
+          <hr />
+
+
+          <p className=" text-[18px] text-white  pt-6 text-center">
+            2024 All rights reserved. &copy;2024 BioCare Bangladesh
+          </p>
+
         </div>
       </div>
-      
+
     </>
   );
 };
--- src/app/component/Navbar/Side/page.js
+++ src/app/component/Navbar/Side/page.js
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import { FaHome, FaUserFriends, FaCog, FaEnvelope } from 'react-icons/fa';
 import { FaCapsules, FaFemale, FaHeartbeat, FaSyringe, FaBaby, FaTooth, FaVial, FaBacon, FaHandsWash, FaLaptopMedical, FaPrescriptionBottleAlt } from 'react-icons/fa';
 import Link from 'next/link';
+import Banner from '../Banner/Banner';
 
 export default function Drawer() {
   const [isOpen, setIsOpen] = useState(false);
 
   const menuItems = [
-    { href: '/', icon: <FaCapsules />, label: 'OTC Medicine', className: "text-blue-600 ml-5" },
+    { href: '/', icon: <FaCapsules />, label: 'OTC Medicine', className: "text-blue-600 ml-5 " },
     { href: '/friends', icon: <FaFemale />, label: 'Women Choice', className: "text-pink-600" },
     { href: '/settings', icon: <FaHeartbeat />, label: 'Sexual Wellness', className: "text-red-600" },
     { href: '/messages', icon: <FaSyringe />, label: 'Diabetic Care', className: "text-green-600" },
@@ -17,12 +18,12 @@ export default function Drawer() {
     { href: '/messages', icon: <FaVial />, label: 'Supplement', className: "text-purple-600" },
     { href: '/messages', icon: <FaBacon />, label: 'Diapers', className: "text-teal-600" },
     { href: '/messages', icon: <FaHandsWash />, label: 'Personal Care', className: "text-orange-600" },
-    { href: '/messages', icon: <FaLaptopMedical />, label: 'Devices', className: "text-blue-600" },
+    { href: '/messages', icon: <FaLaptopMedical />, label: 'Devices', className: "text-blue-600 " },
     { href: '/messages', icon: <FaPrescriptionBottleAlt />, label: 'Prescription Medicine', className: "text-red-600" },
   ];
 
   return (
-    <div className="flex">
+    <div className="flex ">
       {/* Sidebar */}
       <div
         className={`flex flex-col bg-[#f9fafb] h-screen overflow-y-auto transition-width duration-300 ease-in-out ${
@@ -35,11 +36,11 @@ export default function Drawer() {
         >
           ☰
         </button>
-        <nav className="flex flex-col space-x-4 mt-4">
+        <nav className="flex flex-col space-x-4 mt-4 ">
           {menuItems.map((item, index) => (
             <Link href={item.href} key={index} legacyBehavior>
-              <a className={`flex items-center p-2 hover:bg-gray-700 rounded-md ${item.className}`}>
-                <span className="text-2xl">{item.icon}</span>
+              <a className={`flex items-center p-2   hover:bg-gray-700 rounded-md ${item.className}`}>
+                <span className="text-2xl my-2">{item.icon}</span>
                 <span
                   className={`ml-2 transition-opacity text-[#000] duration-300 ${
                     isOpen ? 'opacity-100' : 'opacity-0'
@@ -56,8 +57,8 @@ export default function Drawer() {
       {/* Main Content */}
       <div className="p-6 flex-grow">
         <h1 className="text-2xl">Main Content Area</h1>
-        <p>This is the main content area and write some thing.</p>
       </div>
+     
     </div>
   );
 }
