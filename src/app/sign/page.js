13550fe3d7495063564482921a848f9f577ebf58
Shormily <shormilyrais@gmail.com>
2024-09-26 21:26:36 +0600

    fall

--- src/app/sign/page.js
+++ src/app/sign/page.js
@@ -1,14 +1,14 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { FcDocument, FcFeedback, FcGoogle, FcReading } from 'react-icons/fc';
 import ScrollToTop from 'react-scroll-to-top';
 import { MdKeyboardArrowUp } from 'react-icons/md';
 import { BsGithub } from 'react-icons/bs';
 import "./globals.css";
 // import { useRouter } from 'next/navigation';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
+// import { FiEye, FiEyeOff } from 'react-icons/fi';
 // import { useFormik } from 'formik';
 
 // const initialValues = {
@@ -28,10 +28,13 @@ const page  = () => {
 //             // router.push('/')
 //         }
 //  })
- const [showPassword, setShowPassword] = useState(false); // State to track password visibility
+    // const [showPassword, setShowPassword] = useState(false);
+    // State to track password visibility
 
- const handleClickShowPassword = () => setShowPassword(!showPassword); // Toggle password visibility
- const handleMouseDownPassword = (event) => event.preventDefault(); // Prevent focus loss when clicking
+    // const handleClickShowPassword = () => setShowPassword(!showPassword);
+    // Toggle password visibility
+    // const handleMouseDownPassword = (event) => event.preventDefault();
+    // Prevent focus loss when clicking
 
     return (
         <>
@@ -88,29 +91,19 @@ const page  = () => {
                                     />
                                 </div>
 
-                                <div className="group pt-8 mx-5 relative">
+                                <div className="group pt-8 mx-5">
                                     <span className="icon">
                                         <FcDocument size={20} />
                                     </span>
                                     <input
-                                        className="inputs shadow-lg w-full pr-10"
-                                        type={showPassword ? 'text' : 'password'} // Toggle between text and password
-                                       id='password'
-                                        placeholder="Enter your password"
-                                        // value={values.password}
+                                        className="inputs shadow-lg w-full"
+                                        type="password"
+                                        placeholder="password"
+                                        // value={values.email}
                                         // onChange={handleChange}
+                                        name="email"
                                         required
                                     />
-                                    <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                                        <button
-                                            type="button"
-                                            className="text-gray-500 mt-8"
-                                            onClick={handleClickShowPassword}
-                                            onMouseDown={handleMouseDownPassword}
-                                        >
-                                            {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
-                                        </button>
-                                    </div>
                                 </div>
 
                                 <div className="pt-8 mx-5">
